5ff8ebd1295e33edd65e50d941b804a0d77d6e63
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-10-07 14:37:41 -0500

    Se corrige la ejecución de los escenarios

--- src/test/scala/parabank/LoginTest.scala
+++ src/test/scala/parabank/LoginTest.scala
@@ -26,14 +26,13 @@ class LoginTest extends Simulation{
       .check(status.is(200))
     )
 
-
-
   // 3 Load Scenario
   setUp(
-    loadTest_100.protocols(httpConf),
-    loadTest_200.protocols(httpConf)
+    load_test100.inject(atOnceUsers(100)).protocols(httpConf),
+    load_test200.inject(atOnceUsers(200)).protocols(httpConf)
   ).assertions(
     global.responseTime.mean.lt(2000),
     global.responseTime.mean.lt(5000)
   )
 }
+
