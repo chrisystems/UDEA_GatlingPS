a638c2a9e2d9be71a29447d8539736ac4a8ba81c
Jonathan Granda <jhonatangranda.2002@gmail.com>
2025-10-09 11:25:47 -0500

    correcion data de account

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -8,7 +8,7 @@ object Data{
     val customerId = "12212"
     
     val loanAmount = "20"
-    val fromAccountId = "16230"
+    val fromAccountId = 15564
     val loanDownPayment = "8"
 
     val amountBill = 25
