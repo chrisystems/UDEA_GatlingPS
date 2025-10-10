ec428eb8e96ad10db7a6222705ec67aa904c36bd
chrisystems <51771601+chrisystems@users.noreply.github.com>
2025-10-09 19:53:08 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -1,7 +1,8 @@
 package parabank
 
 object Data{
-    val url= "https://parabank.parasoft.com/parabank/services/bank"
+    val url= "https://para.testar.org/parabank/services/bank"
     val username = "john"
     val password = "demo"
+
 }
